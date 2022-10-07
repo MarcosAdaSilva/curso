89e6ced6d420b1f8f2e1aa11b98fde6610eb5cd2
MarcosAdaSilva <marcaofly@gmail.com>
2022-10-07 00:05:59 -0300

    Fixando

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
-  styleUrls: ['./list-render.component.css']
+  styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
+  animals = [
+    { name: 'Turca', type: 'DOg' },
+    { name: 'Tom', type: 'Cat' },
+    { name: 'Frida', type: 'Dog' },
+    { name: 'Bob', type: 'Horse' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
