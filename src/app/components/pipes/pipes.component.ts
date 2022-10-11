a836ba6a45b97fdf93ca811fa3e90dba337e6390
MarcosAdaSilva <marcaofly@gmail.com>
2022-10-10 23:52:22 -0300

    Fixando

--- src/app/components/pipes/pipes.component.ts
+++ src/app/components/pipes/pipes.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pipes',
   templateUrl: './pipes.component.html',
-  styleUrls: ['./pipes.component.css']
+  styleUrls: ['./pipes.component.css'],
 })
 export class PipesComponent implements OnInit {
+  someText = 'TESTANDO O PIPE OPERATOR';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
